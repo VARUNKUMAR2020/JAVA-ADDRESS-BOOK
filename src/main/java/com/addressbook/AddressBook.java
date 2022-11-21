dfc6517a8190b9f375e2dccd0e1337c87d3d1330
VARUNKUMAR2020 <varunvenkat2020@gmail.com>
2022-11-21 23:29:20 +0530

    Add: Adding new contact

--- src/main/java/com/addressbook/AddressBook.java
+++ src/main/java/com/addressbook/AddressBook.java
@@ -1,12 +1,37 @@
 package com.addressbook;
+import java.util.*;
 
 public class AddressBook {
+    static Scanner input = new Scanner(System.in);
+	static List<Contacts> contactsList;
+	private static String firstName, lastName, city, state, email, phoneNumber, zipCode;
+
+	private static void newContact() {
+		System.out.print("Enter First Name : ");
+		firstName = input.next();
+		System.out.print("Enter Last Name : ");
+		lastName = input.next();
+		System.out.print("Enter Phone : ");
+		phoneNumber = input.next();
+		System.out.print("Enter Email : ");
+		email = input.next();
+		System.out.print("Enter City : ");
+		city = input.next();
+		System.out.print("Enter State : ");
+		state = input.next();
+		System.out.print("Enter Zip : ");
+		zipCode = input.next();
+	}
     public static void main(String[] args) {
         System.out.println("Welcome to Address-Book :)");
 
-        // creating new class for adding contact
-        Contacts contacts = new Contacts("Varun", "Kumar", "9567776443", "varunvenkat2020@gmail.com", "Chennai",
-                "Tamilnadu", "632103");
-        contacts.show();// calling the method to display
+        //creation of array list
+        contactsList = new ArrayList<>();
+		System.out.println("Add new Contact :\n");
+		newContact();// method called
+		Contacts contacts = new Contacts(firstName, lastName, phoneNumber, email, city, state, zipCode);
+		contactsList.add(contacts);//storing the values in the array
+		System.out.println("\nContact Added to the Address Book :\n");
+		System.out.println(contacts);
     }
 }
--- src/main/java/com/addressbook/Contacts.java
+++ src/main/java/com/addressbook/Contacts.java
@@ -3,25 +3,26 @@ package com.addressbook;
 public class Contacts {
     String firstName, lastName, city, state, email, phoneNumber, zipCode;
 
-	public Contacts(String firstName, String lastName, String phoneNumber, String email, String city, String state,
-			String zipCode) {
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.phoneNumber = phoneNumber;
-		this.email = email;
-		this.city = city;
-		this.state = state;
-		this.zipCode = zipCode;
-	}
+    //constructor
+    public Contacts(String firstName, String lastName, String phoneNumber, String email, String city, String state,
+            String zipCode) {
+        this.firstName = firstName;// The this keyword can be used to refer current class instance variable
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.city = city;
+        this.state = state;
+        this.zipCode = zipCode;
+    }
+    
+    //The method does override or implement a method declared
+    @Override
 
-	public void show() {
-		System.out.println("First Name : " + firstName);
-		System.out.println("Last Name  : " + lastName);
-		System.out.println("Phone Number : " + phoneNumber);
-		System.out.println("Email-ID : " + email);
-		System.out.println("City  : " + city);
-		System.out.println("State : " + state);
-		System.out.println("Zip Code : " + zipCode);
-	}
+    //Returns a string representation of the object.
+    public String toString() {
+        return "First Name : '" + firstName + "', Last Name : '" + lastName + "', Phone Number : '" + phoneNumber
+                + "', email ID : '" + email + "', City : '" + city + "', State : '" + state + "', Zipcode : '" + zipCode
+                + "'";
+    }
 
 }
